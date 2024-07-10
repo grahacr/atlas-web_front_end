2a7fa5d6da755ec61ae2eafdc03a6b17daa43614
grahacr <courtney.graham@atlasschool.com>
2024-07-10 01:30:38 -0500

    reusing countprimenumber function to run 100 times and record time it took to execute

---
+++ Javascript_advanced/10-prime.js
@@ -0,0 +1,32 @@
+function countPrimeNumbers() {
+  let count = 0;
+
+  function isPrime(num) {
+    if (num <= 1) return false;
+    if (num === 2) return true;
+    if (num % 2 === 0) return false;
+
+    for (let i = 3; i <= Math.sqrt(num); i += 2) {
+      if (num % i === 0) return false;
+    }
+    return true;
+  }
+
+  for (let j = 2; j <= 100; j++) {
+    if (isPrime(j)) {
+      count++;
+    }
+  }
+  return count;
+}
+
+
+let startTime = performance.now();
+
+for (let i = 0; i < 100; i++) {
+  countPrimeNumbers();
+}
+
+let endTime = performance.now();
+let execTime = endTime - startTime;
+console.log(`Execution time of calculating prime numbers 100 times was ${execTime} milliseconds`);
