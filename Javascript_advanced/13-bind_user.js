bcd667dd944d5da9ce2e8cc9ec867d967a1dda9f
grahacr <courtney.graham@atlasschool.com>
2024-07-10 21:41:54 -0500

    using closure and binding, creating welcome messages using object created

---
+++ Javascript_advanced/13-bind_user.js
@@ -0,0 +1,16 @@
+let user = {
+  hobby: "Calligrapy",
+  favoriteSport: "Hockey",
+  astrologicalSign: "Aries",
+  firstName: "Buillaume",
+  lastName: "Johns",
+  location: "Netherlands",
+  occupation: "Engineer"
+};
+
+function logWelcomeUser(welcomeString) {
+  console.log(`${welcomeString}, ${user.firstName}. Your occupation is: ${user.occupation}`);
+}
+
+const bindLogWelcomeUser = logWelcomeUser.bind(user);
+bindLogWelcomeUser("Welcome");
